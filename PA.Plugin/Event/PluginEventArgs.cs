25e63d7dd59592b60b19b9b993f7abfbbaa5797d
Thomas GERVAIS <thomas.gervais@gmail.com>
2015-08-07 22:33:37 +0200

    [Add] submodule 'PA.Plugin'

---
+++ PA.Plugin/Event/PluginEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PA.Plugin
+{
+    public class PluginEventArgs : EventArgs
+    {
+        public IPlugin Plugin { get; private set; }
+
+        public PluginEventArgs(IPlugin pi)
+            :base()
+        {
+            this.Plugin = pi;
+        }
+    }
+}

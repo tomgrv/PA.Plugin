6a7dd193fe40754f6825a7ee6e00d02ab1494c4f
Thomas G <tomgrv@users.perspikapps.fr>
2021-06-08 23:48:33 +0200

    chore: update meta data

--- common/GlobalAssemblyInfo.cs
+++ common/GlobalAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyCompany("PerspicApps")]
+[assembly: AssemblyCompany("Perspikapps")]
 [assembly: AssemblyProduct("PA.TileList")]
-[assembly: AssemblyCopyright("(c) PA 2015")]
+[assembly: AssemblyCopyright("(c) PA 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
